1bce74b94672d16514661fe2605c8613001a781f
omoologoshola <aladeyeluabdullahi@gmail.com>
2022-09-08 18:42:11 -0700

    a C programs with proper grammar, but the outcome is a piece of art, with a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - print "Programming is like building a multilingual puzzle"
+*
+* Return: 0 if exited properly, non-zer otherwise
+*/
+int main(void)
+{
+puts("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
