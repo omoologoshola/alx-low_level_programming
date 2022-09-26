38d9ed50d0e7dac068d4d04f4ae40cbbcd372b34
omoologoshola <aladeyeluabdullahi@gmail.com>
2022-09-26 16:55:28 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
