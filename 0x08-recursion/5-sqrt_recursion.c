ffdca85f320767415ca16cc126da1c887213680b
omoologoshola <aladeyeluabdullahi@gmail.com>
2022-09-28 13:58:08 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,46 +1,34 @@
 #include "main.h"
+int sqrt_calc(int, int);
+
 /**
- * _sqrt_recursion_wrapper - a wrapper that does the recursion bit
+ * _sqrt_recursion - calculate square root of a number
  *
- * @n : input number
- * @min: minimum number to guess
- * @max: maximum number to guess
- *
- * Return: square root of @n or -1
-*/
-int _sqrt_recursion_wrapper(int n, int min, int max)
+ * @n: value parameter
+ * Return: square root
+ */
+
+int _sqrt_recursion(int n)
 {
-	int guess, guess_squared;
-
-	guess = (min + max) / 2;		/*get guess (g)*/
-	guess_squared = guess * guess;		/*square guess (sg)*/
-
-	if (guess_squared == n)			/*if squared guess == n return guess*/
-		return (guess);
-	else if (min == max)			/*this means n doesn't have a perfect square*/
-		return (-1);
-	else if (guess_squared < n)		/*overshoot g if sg is less than the n*/
-		return (_sqrt_recursion_wrapper(n, guess + 1, max));
-	else					/*undershoot g if sg is more than the n*/
-		return (_sqrt_recursion_wrapper(n, min, guess - 1));
+	if (n == 0)
+		return (0);
+	else
+		return (sqrt_calc(1, n));
 }
 
 /**
- * _sqrt_recursion - a function that returns the natural
- *                   square root of a number
- *
- * @n: input number
- *
- * Return: square root
-*/
-int _sqrt_recursion(int n)
+ * sqrt_calc - to check if the number is a sqaure
+ * @v: value paramater, always starts at 1
+ * @s: value parameter, square number
+ * Return: v if number is a square else -1
+ */
+
+int sqrt_calc(int v, int s)
 {
-	if (n == 1)		/*sqrt(1) == 1*/
-		return (1);
-	else if (n == 0)	/*sqrt(0) == 0*/
-		return (0);
-	else if (n < 0)		/*sqrt(-n) == -1 (NA)*/
+	if (v * v == s)
+		return (v);
+	else if (v * v > s)
 		return (-1);
 	else
-		return (_sqrt_recursion_wrapper(n, 1, n));
+		return (sqrt_calc(v + 1, s));
 }
