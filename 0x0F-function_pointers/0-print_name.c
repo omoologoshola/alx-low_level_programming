977de2e060b66a46a554f8a7ae974a6e1e9876d4
omoologoshola <aladeyeluabdullahi@gmail.com>
2022-10-12 16:11:02 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,15 @@
+#include "function_pointers.h"
 /**
- * print_name - function to print name
- *
- * @name: name to print
- * @f: function pointer that doesn't return anything
- *
- * Return: Always 0 (Success)
-*/
+ * print_name - to print a name from function pointer
+ * @name: to receive the address of the string
+ * @f: function pointer that receives the string argument
+ * Return: void
+ */
 
-void print_name(char *name, void (*f)(char *))
+void print_name(char *name, void(*f)(char *))
 {
-	if (name && f)
+	if (name != NULL && f != NULL)
+	{
 		f(name);
+	}
 }
