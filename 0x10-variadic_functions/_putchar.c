dc7c12ab0b9cca7da68b52589554b2e208fbe143
omoologoshola <aladeyeluabdullahi@gmail.com>
2022-10-13 14:50:53 -0700

    update

--- 0x10-variadic_functions/_putchar.c
+++ 0x10-variadic_functions/_putchar.c
@@ -1,12 +1,13 @@
-#ifndef VAR_FUNC_H
-#define VAR_FUNC_H
+#include <unistd.h>
 
-#include <stdio.h>
-#include <stdarg.h>
-int _putchar(char c);
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
-
-#endif
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
