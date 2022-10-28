e3c6be6b90ff5a35ac70969b25772ee51ef60800
omoologoshola <aladeyeluabdullahi@gmail.com>
2022-10-28 15:58:24 -0700

    0-binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,55 +1,30 @@
-#include <stdio.h>
 #include "main.h"
-int getPower(int);
+#include <stdio.h>
 
 /**
- * getPower - to calculate the product of 2 power of a number
- * @i: the power
- * Return: product
+ * binary_to_uint - convert a binary number to an unsigned int
+ * @b: char string
+ * Return: converted decimal number or 0 if there is an unconvertable char
  */
-
-int getPower(int i)
-{
-	int k, prod = 1;
-
-	for (k = 1; k < i; k++)
-	{
-		prod = prod * 2;
-	}
-	return (prod);
-}
-
-/**
- * binary_to_uint - to convert a binary number to denary
- * @b: stores the address of a binary number
- * Return: a denary value else 0
- */
-
 unsigned int binary_to_uint(const char *b)
 {
-	int i = 0, k, denary_number = 0;
+	unsigned int total, power;
+	int len;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[i] != '\0')
+	for (len = 0; b[len]; len++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
-		i++;
 	}
 
-	k = i--;
-	i = 0;
-
-	while (b[i] != '\0')
+	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
 	{
-		if (b[i] == '1')
-		{
-			denary_number = denary_number + getPower(k);
-		}
-		k--;
-		i++;
+		if (b[len] == '1')
+			total += power;
 	}
-	return (denary_number);
+
+	return (total);
 }
