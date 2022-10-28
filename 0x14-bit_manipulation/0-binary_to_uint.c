2b22ffd8c794f219368b4f1344031ac6b6ae6c90
omoologoshola <aladeyeluabdullahi@gmail.com>
2022-10-28 08:43:05 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,79 +1,55 @@
+#include <stdio.h>
 #include "main.h"
-#include <stddef.h>
+int getPower(int);
 
 /**
- * _atoi - converts chars to integer
- *
- * @c: char to convert
- *
- * Return: converted integer
-*/
-unsigned int _atoi(char c)
+ * getPower - to calculate the product of 2 power of a number
+ * @i: the power
+ * Return: product
+ */
+
+int getPower(int i)
 {
-	return ((unsigned int) c - '0');
+	int k, prod = 1;
+
+	for (k = 1; k < i; k++)
+	{
+		prod = prod * 2;
+	}
+	return (prod);
 }
 
 /**
- * _strlen - gets the length of a string
- *
- * @str: string input
- *
- * Return: string length
-*/
-unsigned int _strlen(const char *str)
-{
-	unsigned int index = 0;
+ * binary_to_uint - to convert a binary number to denary
+ * @b: stores the address of a binary number
+ * Return: a denary value else 0
+ */
 
-	while (str[index] != '\0')
-		index++;
-	return (index);
-}
-
-/**
- * binary_to_uint - a function that converts a binary number
- *                  to an unsigned int
- *
- * @b: string that contains 0 and 1 characters
- *
- * Return: the number converted or 0
- *         if @b contains a character
- *         that is not 0 or 1 or when
- *         @b is null
-*/
 unsigned int binary_to_uint(const char *b)
 {
-	int index;
-	unsigned int result = 0, base2 = 1,  num = 0;
+	int i = 0, k, denary_number = 0;
 
-	/*if b is NULL return 0*/
 	if (b == NULL)
 		return (0);
 
-	#ifdef DEBUG /*print debug statement*/
-	printf("String is %s, and length is %u.\n", b, _strlen(b));
-	#endif
-
-	/*iterate through string*/
-	for (index = _strlen(b) - 1; index >= 0; index--)
+	while (b[i] != '\0')
 	{
-		num = _atoi(b[index]); /*convert char to number*/
-
-		#ifdef DEBUG /*print debug statements*/
-		printf("Number is %u, index is %i and base is %u.\n\n", num, index, base2);
-		#endif
-
-		/*if number is not 0 or 1 return 0*/
-		if (num != 0 && num != 1)
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-
-		result += num * base2; /*enable debug to see it in action*/
-		base2 *= 2;
-
-		#ifdef DEBUG /*print debug statements*/
-		printf("Result is %u.\n", result);
-		#endif
+		i++;
 	}
 
-	return (result);
+	k = i--;
+	i = 0;
 
+	while (b[i] != '\0')
+	{
+		if (b[i] == '1')
+		{
+			denary_number = denary_number + getPower(k);
+		}
+		k--;
+		i++;
+	}
+	return (denary_number);
 }
